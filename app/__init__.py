bde38ba8bba7c6f05a63b4b4924a97a85f8089d2
Marvin Chomba <marvin.chomba24@gmail.com>
2018-11-24 16:43:15 +0300

    Add and register the main blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -25,4 +25,8 @@ def create_app(config_name):
     db.init_app(app)
     login_manager.init_app(app)
 
+    #register blueprints
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
     return app
